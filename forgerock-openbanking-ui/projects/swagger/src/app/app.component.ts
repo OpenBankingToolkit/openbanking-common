495829a0bb0d5396127ebe4b8d33d8db50882cc5
Julien Renaux <julien.renaux@forgerock.com>
2020-03-12 14:47:59 +0100

    adding registration feature flag

--- forgerock-openbanking-ui/projects/swagger/src/app/app.component.ts
+++ forgerock-openbanking-ui/projects/swagger/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Platform } from '@angular/cdk/platform';
 import { DOCUMENT } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 
-import { ForgerockConfigService } from '@forgerock/openbanking-ngx-common/services/forgerock-config';
 import { ForgerockSplashscreenService } from '@forgerock/openbanking-ngx-common/services/forgerock-splashscreen';
 import { ForgerockGDPRService } from '@forgerock/openbanking-ngx-common/gdpr';
 
@@ -14,12 +13,9 @@ import { ForgerockGDPRService } from '@forgerock/openbanking-ngx-common/gdpr';
   `
 })
 export class AppComponent {
-  enableCustomization: string = this.configService.get('enableCustomization');
-
   constructor(
     @Inject(DOCUMENT) private document: any,
     private splashscreenService: ForgerockSplashscreenService,
-    private configService: ForgerockConfigService,
     private platform: Platform,
     private translateService: TranslateService,
     private gdprService: ForgerockGDPRService
